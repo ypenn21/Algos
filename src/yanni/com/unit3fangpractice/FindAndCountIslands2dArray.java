58c52b27d03be1614ecff6065028504ffd4ed7e5
ypenn21 <yannipeng21@gmail.com>
2020-07-16 12:21:41 -0700

    island problem added code to count the biggest island

--- src/yanni/com/unit3fangpractice/FindAndCountIslands2dArray.java
+++ src/yanni/com/unit3fangpractice/FindAndCountIslands2dArray.java
@@ -7,16 +7,25 @@ public class FindAndCountIslands2dArray {
     public Integer findAndCountIslands2dArray(int[][] locations, boolean[][] visted) {
 
         int count=0;
+        int biggest=0;
 
         for(int row=0;row<locations.length;row++){
             for(int col=0;col<locations[row].length;col++){
-                if(!visted[row][col])
-                    count += scanForIslands(row, col, locations, visted);
+                if(!visted[row][col] && locations[row][col] == 1) {
+                    System.out.println("Island: "+scanForIslands(row, col, locations, visted));
+                    //System.out.println("Island: "+this.islandSize);
+                    biggest = Math.max(biggest, this.islandSize);
+                    this.islandSize =0;
+                    count++;
+                }
                 visted[row][col]=true;
             }
         }
+        System.out.println("The biggest land size: "+biggest);
         return count;
     }
+
+    public int islandSize =0;
 
     public Integer scanForIslands(int row, int col, int[][] locations, boolean[][] visted) {
         int count=0;
@@ -24,22 +33,20 @@ public class FindAndCountIslands2dArray {
         int[] scannerCol =  { 0, 0, -1, 1, -1, 1,  1, -1};
 
         if(locations[row][col] == 1) {
+            this.islandSize++;
+            count++;
             // just use a try catch here to help me debug not something i would write in production
-            try {
-                for (int scan = 0; scan < scannerRole.length; scan++) {
-                    if (isItSafe(row + scannerRole[scan], col + scannerCol[scan], locations)
-                            && !visted[row + scannerRole[scan]][col + scannerCol[scan]]) {
-                        visted[row + scannerRole[scan]][col + scannerCol[scan]] = true;
-                        count += scanForIslands(row + scannerRole[scan], col + scannerCol[scan], locations, visted);
-                    } else {
-                        continue;
-                    }
+            for (int scan = 0; scan < scannerRole.length; scan++) {
+                if (isItSafe(row + scannerRole[scan], col + scannerCol[scan], locations)
+                        && !visted[row + scannerRole[scan]][col + scannerCol[scan]]) {
+                    visted[row][col]=true;
+                    visted[row + scannerRole[scan]][col + scannerCol[scan]] = true;
+                    count += scanForIslands(row + scannerRole[scan], col + scannerCol[scan], locations, visted);
+                } else {
+                    continue;
                 }
-                return 1;
-            } catch(Exception e) {
-                System.out.println();
-                throw e;
             }
+            return count;
         }
         return 0;
     }
@@ -59,8 +66,8 @@ public class FindAndCountIslands2dArray {
     public void testFindAndCountIslands2dArray(){
         int[][] map = {
                         {0,0,1,0,1},
-                        {0,1,1,0,0},
-                        {0,0,0,0,1},
+                        {0,0,1,0,0},
+                        {0,0,0,1,1},
                         {1,0,1,1,1},
                         {1,0,1,1,1}
                       };
@@ -73,7 +80,7 @@ public class FindAndCountIslands2dArray {
         };
         int count = findAndCountIslands2dArray(map, visted);
         System.out.println(count);
-        assert(count==4);
+        assert(count==3);
     }
 
 }
